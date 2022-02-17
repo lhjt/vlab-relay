9af6cdafadba6672bbea0ed4e873f1eb18a774b8
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-18 04:29:37 +1100

    feat(server): add logging

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,7 @@ use relay::{
     AutoTestSubmissionRequest, AutoTestSubmissionResponse, SubmissionRequest, SubmissionResponse,
 };
 use tonic::{transport::Server, Request, Response, Status};
+use tracing::info;
 
 pub mod relay {
     tonic::include_proto!("core");
@@ -29,9 +30,12 @@ impl relay::relay_service_server::RelayService for Relay {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
     let addr = "[::1]:50051".parse()?;
     let relay = Relay::default();
 
+    info!("[gRPC] launching gRPC server on {}", addr);
     Server::builder()
         .add_service(relay::relay_service_server::RelayServiceServer::new(relay))
         .serve(addr)
