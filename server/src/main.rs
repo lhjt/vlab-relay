1947821d1870c79edd59d57701411c855442053d
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-18 04:44:30 +1100

    feat: add server scaffolding (#1)
    * feat(server): add initial gRPC server
    
    * feat(server): add logging
    
    * feat(grpc): add unimplemented warning
    
    * feat(server): add base ws server impl

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,1 +1,64 @@
-fn main() {}
+use relay::{
+    AutoTestSubmissionRequest, AutoTestSubmissionResponse, SubmissionRequest, SubmissionResponse,
+};
+use tokio::net::TcpListener;
+use tonic::{transport::Server, Request, Response, Status};
+use tracing::info;
+
+pub mod relay {
+    tonic::include_proto!("core");
+}
+
+#[derive(Debug, Default)]
+pub struct Relay {}
+
+#[tonic::async_trait]
+impl relay::relay_service_server::RelayService for Relay {
+    async fn perform_auto_test(
+        &self,
+        request: Request<AutoTestSubmissionRequest>,
+    ) -> Result<Response<AutoTestSubmissionResponse>, Status> {
+        Err(Status::unimplemented("not implemented"))
+    }
+
+    async fn submit_work(
+        &self,
+        request: Request<SubmissionRequest>,
+    ) -> Result<Response<SubmissionResponse>, Status> {
+        Err(Status::unimplemented("not implemented"))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
+    // websocket server
+    tokio::spawn(async move {
+        let listener = TcpListener::bind("0.0.0.0:50052")
+            .await
+            .expect("failed to bind");
+
+        info!("[ws] listening on {}", "0.0.0.0:50052");
+
+        while let Ok((stream, _)) = listener.accept().await {
+            let peer = stream
+                .peer_addr()
+                .expect("connected streams should have a peer address");
+            info!("[ws] Peer address: {}", peer);
+
+            // deal with the connection
+        }
+    });
+
+    let grpc_addr = "[::1]:50051".parse()?;
+    let relay = Relay::default();
+
+    info!("[gRPC] launching gRPC server on {}", grpc_addr);
+    Server::builder()
+        .add_service(relay::relay_service_server::RelayServiceServer::new(relay))
+        .serve(grpc_addr)
+        .await?;
+
+    Ok(())
+}
