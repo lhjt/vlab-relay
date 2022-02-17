feb16dea62ed9f28d3790c400ac1013408ba3326
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-18 04:42:17 +1100

    feat(server): add base ws server impl

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 use relay::{
     AutoTestSubmissionRequest, AutoTestSubmissionResponse, SubmissionRequest, SubmissionResponse,
 };
+use tokio::net::TcpListener;
 use tonic::{transport::Server, Request, Response, Status};
 use tracing::info;
 
@@ -32,13 +33,31 @@ impl relay::relay_service_server::RelayService for Relay {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    let addr = "[::1]:50051".parse()?;
+    // websocket server
+    tokio::spawn(async move {
+        let listener = TcpListener::bind("0.0.0.0:50052")
+            .await
+            .expect("failed to bind");
+
+        info!("[ws] listening on {}", "0.0.0.0:50052");
+
+        while let Ok((stream, _)) = listener.accept().await {
+            let peer = stream
+                .peer_addr()
+                .expect("connected streams should have a peer address");
+            info!("[ws] Peer address: {}", peer);
+
+            // deal with the connection
+        }
+    });
+
+    let grpc_addr = "[::1]:50051".parse()?;
     let relay = Relay::default();
 
-    info!("[gRPC] launching gRPC server on {}", addr);
+    info!("[gRPC] launching gRPC server on {}", grpc_addr);
     Server::builder()
         .add_service(relay::relay_service_server::RelayServiceServer::new(relay))
-        .serve(addr)
+        .serve(grpc_addr)
         .await?;
 
     Ok(())
