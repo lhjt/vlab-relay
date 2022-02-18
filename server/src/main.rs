f6fe871dd8cd0597798bc6a462192352d9946e6b
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-19 01:24:36 +1100

    fix: ci warnings

--- server/src/main.rs
+++ server/src/main.rs
@@ -19,14 +19,14 @@ pub struct Relay {}
 impl relay::relay_service_server::RelayService for Relay {
     async fn perform_auto_test(
         &self,
-        request: Request<AutoTestSubmissionRequest>,
+        _request: Request<AutoTestSubmissionRequest>,
     ) -> Result<Response<AutoTestSubmissionResponse>, Status> {
         Err(Status::unimplemented("not implemented"))
     }
 
     async fn submit_work(
         &self,
-        request: Request<SubmissionRequest>,
+        _request: Request<SubmissionRequest>,
     ) -> Result<Response<SubmissionResponse>, Status> {
         Err(Status::unimplemented("not implemented"))
     }
