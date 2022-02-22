c85315c8b31de3663347e6f5fc55538a6012d054
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-23 02:37:06 +1100

    chore(server): move ws login to own module

--- server/src/ws/messaging.rs
+++ server/src/ws/messaging.rs
@@ -1,11 +1,11 @@
 use std::net::SocketAddr;
 
-use prost::{bytes::Bytes, Message};
 use tokio_tungstenite::tungstenite::Message as WSMessage;
-use tracing::{info, instrument, warn};
+use tracing::{info, instrument};
 
-use super::{PeerData, PeerMap};
-use crate::relay::ws_extensions::InitFrame;
+use super::PeerMap;
+
+mod operations;
 
 /// Handle a message from a peer. This is called for each message received from
 /// each peer.
@@ -26,43 +26,6 @@ pub(crate) async fn handle_message(peer_map: PeerMap, msg: WSMessage, address: S
             // peer is registered, handle the message
             info!("[ws] received message from peer user: `{}`", pd.username);
         },
-        None => {
-            // attempt to register the peer
-            // check the message type
-            if let WSMessage::Binary(data) = msg {
-                // attempt to decode it into an InitFrame
-                if let Ok(frame) = InitFrame::decode(Bytes::from(data)) {
-                    // TODO: check if the frame is valid (i.e. validate code with database and
-                    // all that)
-                    // We will assume it's fine for the time being
-                    let data = PeerData {
-                        token:    frame.token,
-                        username: frame.zid,
-                    };
-
-                    if data.token.is_empty() {
-                        warn!("[ws] peer attempted to register with invalid token");
-                        peer.close_with_policy();
-                        return;
-                    }
-
-                    // store the data
-                    info!("[ws] peer `{}`", data.username);
-                    peer.data = Some(data);
-                } else {
-                    warn!("[ws] failed to decode init frame from peer: {}", address);
-                    // TODO: we should be closing with policy, but for testing
-                    // purposes we will auth the client
-                    // peer.close_with_policy()
-                    peer.data = Some(PeerData {
-                        token:    "dummy".to_string(),
-                        username: "dummy".to_string(),
-                    });
-                }
-            } else {
-                // the peer is not attempting to validate; close the connection
-                peer.close_with_policy();
-            }
-        },
+        None => operations::handle_registration(peer, msg, address),
     }
 }
---
+++ server/src/ws/messaging/operations.rs
@@ -0,0 +1,48 @@
+use std::net::SocketAddr;
+
+use prost::bytes::Bytes;
+use tokio_tungstenite::tungstenite::Message;
+use tracing::{info, warn};
+
+use super::super::{Peer, PeerData};
+use crate::relay::ws_extensions::InitFrame;
+
+/// Handle a registration message from a peer.
+pub(crate) fn handle_registration(peer: &mut Peer, msg: Message, address: SocketAddr) {
+    // attempt to register the peer
+    // check the message type
+    if let Message::Binary(data) = msg {
+        // attempt to decode it into an InitFrame
+        if let Ok(frame) = <InitFrame as prost::Message>::decode(Bytes::from(data)) {
+            // TODO: check if the frame is valid (i.e. validate code with database and
+            // all that)
+            // We will assume it's fine for the time being
+            let data = PeerData {
+                token:    frame.token,
+                username: frame.zid,
+            };
+
+            if data.token.is_empty() {
+                warn!("[ws] peer attempted to register with invalid token");
+                peer.close_with_policy();
+                return;
+            }
+
+            // store the data
+            info!("[ws] peer `{}`", data.username);
+            peer.data = Some(data);
+        } else {
+            warn!("[ws] failed to decode init frame from peer: {}", address);
+            // TODO: we should be closing with policy, but for testing
+            // purposes we will auth the client
+            // peer.close_with_policy()
+            peer.data = Some(PeerData {
+                token:    "dummy".to_string(),
+                username: "dummy".to_string(),
+            });
+        }
+    } else {
+        // the peer is not attempting to validate; close the connection
+        peer.close_with_policy();
+    }
+}
