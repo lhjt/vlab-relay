83274bbc85024467d2d276029a566f6d38d7a4b5
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-23 03:36:26 +1100

    feat(server): add ws registration timeout

--- server/src/ws/mod.rs
+++ server/src/ws/mod.rs
@@ -9,7 +9,7 @@ use futures::{
 };
 use tokio::{net::TcpStream, sync::Mutex};
 use tokio_tungstenite::tungstenite::Message;
-use tracing::{debug, info, instrument};
+use tracing::{debug, info, instrument, warn};
 
 use self::models::Peer;
 
@@ -59,6 +59,27 @@ pub(crate) async fn handle_connection(peer_map: PeerMap, stream: TcpStream, addr
     let receive_from_others = rx.map(Ok).forward(outgoing);
 
     pin_mut!(broadcast_incoming, receive_from_others);
+
+    // handle registration timeout
+    let peers = Arc::clone(&peer_map);
+    tokio::spawn(async move {
+        // give the client 3 seconds to register itself
+        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
+        // if the client hasn't registered itself, close the connection
+        let peers = peers.lock().await;
+        match peers.get(&address) {
+            Some(peer) if peer.data.is_none() => {
+                warn!(
+                    "[ws] client didn't register itself; closing connection: {}",
+                    address
+                );
+                peer.close_with_policy();
+            },
+            _ => {},
+        }
+    });
+
     future::select(broadcast_incoming, receive_from_others).await;
 
     info!("[ws] connection closed: {}", address);
