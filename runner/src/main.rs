c3ea3c2597a4e2781de2345c617dec3d35415f31
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-25 04:53:04 +1100

    feat(runner): init

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use clap::Parser;
+
+/// A VLab relay runner. This app should run on your VLab instance, under your
+/// account name. It is highly recommended that you run this in some sort of
+/// detachable interface, such as zellij or screen.
+#[derive(Parser, Debug)]
+#[clap(name = "vlab relay runner", author, version, about, long_about = None, verbatim_doc_comment)]
+struct Args {}
+
+fn main() { Args::parse(); }
