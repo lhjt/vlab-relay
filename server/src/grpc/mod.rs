37be1c4cc5e80b325ed280738c508ba57f545eb1
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-25 01:23:25 +1100

    feat(server): add auth handling to gRPC

--- server/src/grpc/mod.rs
+++ server/src/grpc/mod.rs
@@ -17,16 +17,23 @@ impl RelayService for Relay {
         &self,
         request: Request<CommandRequest>,
     ) -> Result<Response<CommandResponse>, Status> {
-        let meta = request.metadata().clone();
-        let zid = meta.get("zid").unwrap().to_str().unwrap();
+        let zid = match interceptors::get_zid(request.metadata()).await {
+            Some(z) => z,
+            None => {
+                return Err(Status::new(
+                    tonic::Code::Unauthenticated,
+                    "You must be authenticated to use this service.",
+                ));
+            },
+        };
 
         let mgr = MANAGER.get().unwrap();
-        let result = mgr.forward_task(zid, request.into_inner()).await;
+        let result = mgr.forward_task(&zid, request.into_inner()).await;
 
         match result {
             Ok(v) => Ok(Response::new(v)),
             Err(e) => {
-                error!("{:?}", e);
+                error!("failed to forward task: {:?}", e);
                 Err(Status::unavailable(
                     // TODO: more detailed error messages
                     "failed to forward task; please try again later",
