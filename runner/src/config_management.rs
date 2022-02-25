9620a5d5d2bc07ed479122d2a1246827a86be5ec
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-26 03:29:29 +1100

    wip(runner): add ws baseline

--- runner/src/config_management.rs
+++ runner/src/config_management.rs
@@ -1,9 +1,20 @@
 use dialoguer::{theme::ColorfulTheme, Confirm, Input, Password};
 
+#[derive(Debug, Clone)]
 pub(crate) struct Configuration {
     pub(crate) host:   String,
     pub(crate) secure: bool,
     pub(crate) token:  String,
+}
+
+impl Configuration {
+    pub(crate) fn get_url(&self) -> String {
+        if self.secure {
+            format!("wss://{}", self.host)
+        } else {
+            format!("ws://{}", self.host)
+        }
+    }
 }
 
 pub(crate) fn get_config() -> Configuration {
@@ -22,6 +33,8 @@ pub(crate) fn get_config() -> Configuration {
         .with_prompt("Please enter your token")
         .interact()
         .unwrap();
+
+    println!();
 
     Configuration {
         host,
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
+use colored::Colorize;
+use futures::{pin_mut, StreamExt, TryStreamExt};
 use human_panic::setup_panic;
+use log::{error, info, warn};
 
 /// A VLab relay runner. This app should run on your VLab instance, under your
 /// account name. It is highly recommended that you run this in some sort of
@@ -11,8 +14,10 @@ struct Args {}
 mod config_management;
 mod startup;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     setup_panic!();
+    simple_logger::init_with_level(log::Level::Info).expect("failed to initialize logger");
     Args::parse();
 
     // header output
@@ -23,4 +28,43 @@ fn main() {
 
     // run relay
     // TODO: actual application logic
+
+    loop {
+        // attempt to connect to the relay
+        let spinner =
+            spinners::Spinner::new(spinners::Spinners::Dots, "Connecting to relay".to_string());
+        let connection = tokio_tungstenite::connect_async(config.get_url()).await;
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        match connection {
+            // if we failed to connect, wait a bit and try again
+            Err(e) => {
+                spinner.stop_with_message("❌ Failed to connect to relay\n".red().to_string());
+                error!(
+                    "failed to connect to relay; will attempt to reconnect in 5 seconds: {}",
+                    e
+                );
+                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+            },
+            Ok((stream, ..)) => {
+                spinner.stop_with_message("✔ Connected to relay\n".green().to_string());
+                let (tx, rx) = futures::channel::mpsc::unbounded();
+                let (write, read) = stream.split();
+
+                let write = rx.map(Ok).forward(write);
+                let messages = read.try_for_each(|msg| async {
+                    let data = msg.into_data();
+                    info!("received message: {:?}", data);
+                    Ok(())
+                });
+
+                pin_mut!(write, messages);
+
+                futures::future::select(write, messages).await;
+
+                warn!("disconnected from relay; will attempt to reconnect in 5 seconds");
+                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+            },
+        }
+    }
 }
