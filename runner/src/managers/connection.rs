e8f470e5e4d5dd6913e5be5b25814a9bcc142a04
Jared L <48422312+lhjt@users.noreply.github.com>
2022-02-27 02:33:52 +1100

    feat(runner): add basic task handling

--- runner/src/managers/connection.rs
+++ runner/src/managers/connection.rs
@@ -1,52 +1,32 @@
 use std::time::Duration;
 
 use colored::Colorize;
-use futures::{
-    channel::mpsc::{self, UnboundedSender},
-    pin_mut,
-    StreamExt,
-    TryStreamExt,
-};
+use futures::{channel::mpsc, future, pin_mut, StreamExt, TryStreamExt};
 use log::{error, info, warn};
+use prost::bytes::Bytes;
 use spinners::{Spinner, Spinners};
 use tokio_tungstenite::tungstenite::Message;
 
-use crate::config_management::Configuration;
+use crate::{
+    config_management::Configuration,
+    managers::tasks::Task,
+    relay::ws_extensions::{socket_frame::Data, SocketFrame},
+};
 
 /// A manager to manage all network activity with the relay.
 #[derive(Debug, Clone)]
 pub(crate) struct ConnectionManager {
-    config:     Configuration,
-    write_sink: Option<UnboundedSender<Message>>,
+    config: Configuration,
 }
 
 impl ConnectionManager {
-    pub(crate) fn new(config: Configuration) -> Self {
-        Self {
-            config,
-            write_sink: None,
-        }
-    }
-
-    pub(crate) fn send_message(&self, message: Message) {
-        match &self.write_sink {
-            Some(sink) => match sink.unbounded_send(message) {
-                Ok(_) => {},
-                Err(e) => {
-                    error!("failed to send message: {}", e);
-                },
-            },
-            None => {
-                error!("no write sink available");
-            },
-        };
-    }
+    pub(crate) fn new(config: Configuration) -> Self { Self { config } }
 
     pub(crate) async fn connect_and_listen(&mut self) {
         loop {
             // attempt to connect to the relay
             let spinner = Spinner::new(Spinners::Dots, "Connecting to relay".to_string());
-            let connection = tokio_tungstenite::connect_async(self.config.get_url()).await;
+            let connection = tokio_tungstenite::connect_async(self.config.get_url().clone()).await;
             tokio::time::sleep(Duration::from_secs(1)).await;
 
             match connection {
@@ -65,11 +45,64 @@ impl ConnectionManager {
                     let (write, read) = stream.split();
 
                     let write = rx.map(Ok).forward(write);
-                    self.write_sink = Some(tx);
-                    let messages = read.try_for_each(|msg| async {
-                        let data = msg.into_data();
-                        info!("received message: {:?}", data);
-                        Ok(())
+                    let messages = read.try_for_each(|msg| {
+                        let tx = tx.clone();
+
+                        tokio::spawn(async move {
+                            // determine the type of msg received
+                            match msg {
+                                Message::Binary(data) => {
+                                    // attempt to parse the message as a task request
+                                    match <SocketFrame as prost::Message>::decode(Bytes::from(data))
+                                    {
+                                        Ok(frame) => {
+                                            // if we successfully parsed the message, then we can
+                                            // process it
+                                            match frame.data {
+                                                Some(data) => {
+                                                    if let Data::TaskRequest(req) = data {
+                                                        info!("received task request: {}", req.id);
+
+                                                        let response =
+                                                            Task::from(req).execute().await;
+                                                        info!(
+                                                            "sending task response: {}",
+                                                            response.id
+                                                        );
+                                                        let return_frame = SocketFrame {
+                                                            data: Some(Data::TaskResponse(
+                                                                response,
+                                                            )),
+                                                        };
+                                                        tx.unbounded_send(Message::Binary(
+                                                            prost::Message::encode_to_vec(
+                                                                &return_frame,
+                                                            ),
+                                                        ))
+                                                        .unwrap();
+                                                    }
+                                                },
+                                                None => {
+                                                    error!("received invalid message from relay");
+                                                },
+                                            }
+                                        },
+                                        Err(e) => {
+                                            error!("failed to parse message: {}", e);
+                                        },
+                                    }
+                                },
+                                Message::Close(_) => {
+                                    warn!("received close message from relay");
+                                },
+                                _ => {
+                                    // should not be receiving any other message types
+                                    warn!("received unexpected message type");
+                                },
+                            }
+                        });
+
+                        future::ok(())
                     });
 
                     pin_mut!(write, messages);
@@ -77,7 +110,6 @@ impl ConnectionManager {
                     futures::future::select(write, messages).await;
 
                     warn!("disconnected from relay; will attempt to reconnect in 5 seconds");
-                    self.write_sink = None;
                     tokio::time::sleep(Duration::from_secs(5)).await;
                 },
             }
--- runner/src/managers/tasks.rs
+++ runner/src/managers/tasks.rs
@@ -1,1 +1,130 @@
-pub(crate) struct TaskManager {}
+use std::{
+    fs::DirBuilder,
+    path::{Path, PathBuf},
+};
+
+use colored::Colorize;
+use log::{error, info};
+
+use crate::relay::{
+    core::{CommandRequest, CommandResponse, Directory},
+    ws_extensions::{TaskRequest, TaskResponse},
+};
+
+#[derive(Debug)]
+pub(crate) struct Task {
+    pub(crate) id:      String,
+    pub(crate) request: CommandRequest,
+}
+
+macro_rules! return_error_response {
+    ($id:expr, $msg:expr) => {{
+        error!($msg);
+        return TaskResponse {
+            id:       $id,
+            response: Some(CommandResponse {
+                output:    format!("operation failed: {}", $msg.red().to_string()),
+                exit_code: -1,
+            }),
+        }}
+    };
+    ($id:expr, $msg:expr, $($arg:expr),*) => {{
+        error!($msg, $($arg),*);
+        return TaskResponse {
+            id:       $id,
+            response: Some(CommandResponse {
+                output:    format!("operation failed: {}", format!($msg, $($arg),*)).red().to_string(),
+                exit_code: -1,
+            }),
+        }}
+    };
+}
+
+impl Task {
+    pub(crate) async fn execute(self) -> TaskResponse {
+        info!("executing task: {}", self.id);
+        // create a new temporary folder and cd into it
+        let folder_name = format!("runner-tmp-{}", self.id);
+
+        // create all of the relevant files in this directory
+        let root_dir = match self.request.directory {
+            Some(mut d) => {
+                // the root directory will contain everything else, so we will name it the temp
+                // folder
+                d.name = folder_name.clone();
+                d
+            },
+            None => return_error_response!(self.id, "no directory specified"),
+        };
+
+        // realise the directory
+        match root_dir.realise(Path::new(".")) {
+            Ok(_) => {},
+            Err(e) => return_error_response!(self.id, "failed to create working directory: {}", e),
+        }
+
+        // all files have been created; now we can execute the command
+        let child = match tokio::process::Command::new(self.request.command)
+            .args(self.request.arguments)
+            .current_dir(Path::new(&folder_name))
+            .spawn()
+        {
+            Ok(r) => r,
+            Err(e) => return_error_response!(self.id, "failed to execute command: {}", e),
+        };
+
+        // wait for the child to finish
+        match child.wait_with_output().await {
+            Ok(r) => {
+                let status = r.status.code().unwrap_or(1);
+
+                TaskResponse {
+                    id:       self.id,
+                    response: Some(CommandResponse {
+                        // TODO: decide how to handle stdout/stderr together
+                        output:    String::from_utf8_lossy(&r.stdout).to_string()
+                            + &String::from_utf8_lossy(&r.stderr).to_string(),
+                        exit_code: status as i64,
+                    }),
+                }
+            },
+            Err(e) => {
+                return_error_response!(self.id, "failed to wait for command to finish: {}", e)
+            },
+        }
+    }
+}
+
+impl Directory {
+    /// Creates the directory and all files and directories in it.
+    pub(crate) fn realise(self, root: impl Into<PathBuf>) -> Result<(), std::io::Error> {
+        // create each file in this directory
+        // create this intial directory
+        let path = Path::join(&root.into(), self.name);
+        DirBuilder::new().recursive(true).create(&path)?;
+
+        // create each file in this directory
+        for file in self.files.into_iter() {
+            std::fs::write(Path::join(&path, file.file_name), file.data)?;
+        }
+
+        // create each directory in this directory and realise it
+        for dir in self.directories.into_iter() {
+            dir.realise(&path)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl From<TaskRequest> for Task {
+    fn from(cr: TaskRequest) -> Self {
+        Self {
+            id:      cr.id,
+            request: cr.command.unwrap_or_else(|| {
+                error!("received a task request without a command");
+                panic!()
+            }),
+        }
+    }
+}
